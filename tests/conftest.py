7f05d4d812c6291416af961c2fe8c4b95652c78c
Will Dean <william.dean@free-now.com>
2023-07-10 20:05:53 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,13 @@ import numpy as np
 import pandas as pd
 
 
+rng = np.random.default_rng(42)
+
+
 @pytest.fixture
 def df() -> pd.DataFrame:
     n_samples = 100
-    data = np.random.normal(size=(n_samples, 2))
+    data = rng.normal(size=(n_samples, 2))
     frame = pd.DataFrame(data, columns=["x", "y"])
     frame["group"] = np.random.choice(["A", "B"], size=n_samples)
 
@@ -17,7 +20,7 @@ def df() -> pd.DataFrame:
 @pytest.fixture
 def series() -> pd.Series:
     n_samples = 100
-    data = np.random.normal(size=n_samples)
+    data = rng.normal(size=n_samples)
     series = pd.Series(data, name="x")
 
     return series
